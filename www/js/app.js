f82760a4d0d412547e9e1e4f2ef85b4cacb43bb9
ayush <ayushkumarsingh97@gmail.com>
2016-04-12 20:40:07 +0530

    chnages

--- www/js/app.js
+++ www/js/app.js
@@ -6,6 +6,25 @@
 var App = angular.module('starter', ['ionic']);
 
 App.service("imdb",["$http","$log",imdb]);
+angular.module('services', [])
+.service('UserService', function() {
+  // For the purpose of this example I will store user data on ionic local storage but you should save it on a database
+
+  var setUser = function(user_data) {
+    window.localStorage.starter_google_user = JSON.stringify(user_data);
+  };
+
+  var getUser = function(){
+    return JSON.parse(window.localStorage.starter_google_user || '{}');
+  };
+
+  return {
+    getUser: getUser,
+    setUser: setUser
+  };
+});
+
+
 App.controller("AppCtrl",["$scope","$log","imdb",AppCtrl]);
 
 /*function AppCtrl($scope,$log,imdb){
@@ -101,3 +120,38 @@ App.run(function($ionicPlatform) {
     }
   });
 })
+
+
+
+App.controller('WelcomeCtrl', function($scope, $state, UserService, $ionicLoading) {
+  // This method is executed when the user press the "Sign in with Google" button
+  $scope.googleSignIn = function() {
+    $ionicLoading.show({
+      template: 'Logging in...'
+    });
+
+    window.plugins.googleplus.login(
+      {},
+      function (user_data) {
+        // For the purpose of this example I will store user data on local storage
+        UserService.setUser({
+          userID: user_data.userId,
+          name: user_data.displayName,
+          email: user_data.email,
+          picture: user_data.imageUrl,
+          accessToken: user_data.accessToken,
+          idToken: user_data.idToken
+        });
+
+        $ionicLoading.hide();
+        $state.go('app.home');
+      },
+      function (msg) {
+        $ionicLoading.hide();
+      }
+    );
+  };
+})
+
+
+
